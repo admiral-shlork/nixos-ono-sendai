52ea0b10c191ea93146113c3fb6c62ffd9c6e363
admiral-shlork <vvhtvr@protonmail.ch>
2025-02-10 19:43:25 +0000

    removed override for version of brlaser (#1)
    * removed override for version of brlaser
    
    * amended printing config
    
    * removed a stray z

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    brlaser_nixpkgs.url = "github:ahydronous/nixpkgs/e4b52e42c6f6ededdf64a412393fc3d6bd681555";
     veracrypt_nixpkgs.url = "github:NixOS/nixpkgs?rev=50165c4f7eb48ce82bd063e1fb8047a0f515f8ce";
 
     # use the following for unstable:
@@ -15,7 +14,7 @@
     # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
   };
 
-  outputs = { self, nixpkgs, home-manager, brlaser_nixpkgs, veracrypt_nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, veracrypt_nixpkgs, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -35,7 +34,7 @@
             }
             { 
               _module.args = {
-                inherit brlaser_nixpkgs veracrypt_nixpkgs;
+                inherit veracrypt_nixpkgs;
               };
             }
 
