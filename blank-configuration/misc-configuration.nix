08773663a14072084849a91e274192a7011aab26
admiral-shlork <vvhtvr@protonmail.ch>
2025-08-02 09:50:02 +0100

    fixes for Docker CUDA support

--- blank-configuration/misc-configuration.nix
+++ blank-configuration/misc-configuration.nix
@@ -31,7 +31,7 @@
     home = "/home/whatever";
     isNormalUser = true;
     description = "whatever";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
     ];
   };
